47aa9ce02c705ce027faebc084f4cc5df2b1f56c
Amir <amir.fayzullin@gmail.com>
2021-12-27 02:15:42 +0500

    comments for gameReducer
    store removed from window scope

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './components/App/App';
 import {Provider} from "react-redux";
 import {store} from "./store/store";
-window.store = store;
+
 ReactDOM.render(
   <React.StrictMode>
       <Provider store={store}>
--- src/store/gameReducer.js
+++ src/store/gameReducer.js
@@ -2,16 +2,17 @@ import {GameManagerFactory} from "../Game/GameManager";
 
 const initialState = {
     managerFactory: new GameManagerFactory(),
-    manager: null,
-    gameState: null,
-    me: null,
-    enemy: null,
-    enemyField: null,
-    myField: null,
-    initializingPlayer: null,
-    gameLaunched: false,
+    manager: null,                          // instance of GameManager
+    gameState: null,                        // state of game from GameManager
+    me: null,                               // real player (instance of Player)
+    enemy: null,                            // AI player (instance of AIPlayer)
+    enemyField: null,                       // Field instance of AIPlayer
+    myField: null,                          // Field instance of Player
+    initializingPlayer: null,               // current initializing player (instance of Player)
+    gameLaunched: false,                    // tracks if game started or not
 };
 
+// actions types
 const START_NEW_GAME = "GAME/START_NEW_GAME";
 const INITIALIZE_NEXT_PLAYER = "GAME/INITIALIZE_NEW_PLAYER";
 const PERFORM_HIT = "GAME/PERFORM_HIT";
@@ -20,21 +21,25 @@ const HANDLE_MANAGER_UPDATE = "GAME/HANDLE_MANAGER_UPDATE";
 const gameReducer = (state = initialState, action) => {
     let newState;
     switch (action.type) {
+        // performs hit of real player
         case PERFORM_HIT:
             let player = state.manager.getRealPlayer();
             player.performHit(action.row, action.column);
             newState = {...state};
             break;
 
+        // initializes player (name is given in action payload)
         case INITIALIZE_NEXT_PLAYER:
             state.manager.initializeNextPlayer(action.name);
             newState = {...state};
             break;
 
+        // handles game manager update
         case HANDLE_MANAGER_UPDATE:
             newState = {...state};
             break;
 
+        // starts new game with new game manager(given in action payload)
         case START_NEW_GAME:
             newState = {
                 ...initialState,
@@ -45,6 +50,7 @@ const gameReducer = (state = initialState, action) => {
         default:
             return state;
     }
+
 
     return {
         ...newState,
@@ -57,6 +63,7 @@ const gameReducer = (state = initialState, action) => {
     }
 };
 
+// actions creators
 export const startNewGame = (manager) => ({type: START_NEW_GAME, manager});
 export const initializeNextPlayer = (name) => ({type: INITIALIZE_NEXT_PLAYER, name});
 export const performHit = (row, column) => ({type: PERFORM_HIT, row, column});
