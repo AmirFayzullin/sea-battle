18ccfd4da4ef6b3ac9fe3b4d9533a1c5f1e7939c
Amir <amir.fayzullin@gmail.com>
2021-12-27 00:18:37 +0500

    Updates

--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers, createStore} from "redux";
-import gameReducer from "./gameReducer";
-
-let reducers = {
-    game: gameReducer,
-};
-
-export let store = createStore(combineReducers(reducers));
